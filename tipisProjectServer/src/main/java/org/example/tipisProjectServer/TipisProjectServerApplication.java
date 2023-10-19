eab269564f5f9ba2e9487cf28e663aada405d9b0
Yuri Mikhaylov <yura_alex2004@mail.ru>
2023-10-19 22:44:09 +0300

    add empty project java spring

---
+++ tipisProjectServer/src/main/java/org/example/tipisProjectServer/TipisProjectServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.tipisProjectServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipisProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipisProjectServerApplication.class, args);
+	}
+
+}
---
+++ tipisProjectServer/src/test/java/org/example/tipisProjectServer/TipisProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tipisProjectServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipisProjectServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
